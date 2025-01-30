83983b179a95b7728561906898e418704e83cf76
guilhrmxavier <guilherme.xavier@platfomrbuilder>
2025-01-30 10:30:01 -0300

    Adicionando validacao api externa no cadastro de personagens

---
+++ src/main/java/builders/desafio/tecnico/api_personagens_sw/dto/DadoCountResponseSwapi.java
@@ -0,0 +1,4 @@
+package builders.desafio.tecnico.api_personagens_sw.dto;
+
+public record DadoCountResponseSwapi(Integer count) {
+}
--- src/main/java/builders/desafio/tecnico/api_personagens_sw/service/PersonagemService.java
+++ src/main/java/builders/desafio/tecnico/api_personagens_sw/service/PersonagemService.java
@@ -2,11 +2,14 @@ package builders.desafio.tecnico.api_personagens_sw.service;
 
 import builders.desafio.tecnico.api_personagens_sw.dto.DadosCadastroPersonagem;
 import builders.desafio.tecnico.api_personagens_sw.dto.DadosRegistroPersonagem;
+import builders.desafio.tecnico.api_personagens_sw.dto.DadoCountResponseSwapi;
 import builders.desafio.tecnico.api_personagens_sw.model.Personagem;
 import builders.desafio.tecnico.api_personagens_sw.repository.PersonagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -23,9 +26,13 @@ public class PersonagemService {
     }
 
     public DadosRegistroPersonagem cadastrarNovoPersonagem(DadosCadastroPersonagem dados) {
-        if (this.repository.existsByNome(dados.nome())) {
+        String nome = dados.nome().replace(" ", "%20");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<DadoCountResponseSwapi> response = restTemplate.getForEntity("https://swapi.dev/api/people?search=" + nome, DadoCountResponseSwapi.class);
+
+        if (response.getBody().count() >= 1 || this.repository.existsByNome(dados.nome()))
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Personagem já cadastrado.");
-        }
 
         return new DadosRegistroPersonagem(this.repository.save(new Personagem(dados)));
     }
